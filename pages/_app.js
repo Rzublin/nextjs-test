989c36706636dd6c105b4ecce36a4c1251269058
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-05 19:47:02 +0200

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,25 @@
+// import App from 'next/app'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <h3>Header</h3>
+      <Component {...pageProps} />
+      <h3>Footer</h3>
+    </>
+  );
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// MyApp.getInitialProps = async (appContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default MyApp;

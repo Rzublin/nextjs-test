4bd82a0236c9a28208f151e496cbfa6e081742c2
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-11 12:35:55 +0200

    feat: add app shell

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 // import App from 'next/app'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <h3>Header</h3>
+      <Header />
       <Component {...pageProps} />
-      <h3>Footer</h3>
+      <Footer />
     </>
   );
 }

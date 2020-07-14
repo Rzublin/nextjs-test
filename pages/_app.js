10c16a2f788dfeea0a9ec2b4e203a60f1522b2c6
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-07-14 18:03:39 +0200

    feat: add emotion global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
-// import App from 'next/app'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { ThemeProvider } from "emotion-theming";
+import GlobalStyles from "../components/GlobalStyles/GlobalStyles";
+
+const theme = {
+  colors: {
+    primary: "#ff0000",
+  },
+};
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
     </>
   );
 }
